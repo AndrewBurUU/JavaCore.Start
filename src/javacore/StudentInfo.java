a0c95455c267c1664ff8ec9bdf988db8801922f7
AndrewBurUU <andriv80@gmail.com>
2021-08-06 22:06:18 +0800

    first step to github

---
+++ src/javacore/StudentInfo.java
@@ -0,0 +1,10 @@
+package javacore;
+
+import java.util.Arrays;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrew Johnson");
+        System.out.println("2021.08.03");
+    }
+}
